a8c4ac973e84a7ac6e99ac57a49a6bd2c527ba24
n9vaesss <caioo24332433@gmail.com>
2024-05-06 10:19:48 -0300

    grunt

---
+++ GRUNT/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            }
+        }
+    })
+    grunt.registerTask('olagrunt', function () {
+        const done = this.async();
+        setTimeout(function () {
+            console.log('ola grunt');
+            done();
+        }, 3000)
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['olagrunt', 'less', 'sass']);
+}
